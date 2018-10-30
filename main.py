84507ad062e1fb701c4a770f4e9d964c04876605
Pooja Ranawade <poojaranawade@users.noreply.github.com>
2018-10-30 14:05:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ Created on Sat Oct 27 17:37:40 2018
 from flask import Flask, render_template, jsonify, request
 import pandas as pd
 import numpy as np
-import os
 
 
 class items:
@@ -19,7 +18,8 @@ class items:
         self.name = row['primaryTitle']
         self.releaseYear = int(row['startYear'])
         self.ratingLevel = 'Not Adult' if row['isAdult'] == '1' else 'Adult'
-        self.genres = row['genres'].split(',') if row['genres'] is not np.nan else []
+        self.genres = row['genres'].split(
+            ',') if row['genres'] is not np.nan else []
         self.time = str(row['runtimeMinutes']) + ' minutes'
 
     def serialize(self):
@@ -98,6 +98,43 @@ class Flask_app:
                                per_page=remain, total_results=total)
             return "search.html", final[total_pages * self.per_page:], res_list
 
+    def search_title_type(self, title, page, type_item):
+        if page < 0:
+            page = 0
+        if type_item == 'show':
+            type_item = 'tv'
+        rows = self.data.loc[
+            (self.data['primaryTitle'].str.contains(str(title).title())) &
+            (self.data['titleType'].str.contains(type_item))]
+        total = rows.shape[0]
+
+        if total == 0:
+            response = jsonify(status=422, response={
+                               'val': 'similar title not present in dat'})
+            return '', '', response
+
+        total_pages = total // self.per_page
+        remain = total % self.per_page
+        start = ((page - 1) * self.per_page) if page > 1 else 0
+        end = start + self.per_page
+
+        print(page, total, total_pages, start, end, remain)
+        final = []
+        res_list = []
+        for index, row in rows.iterrows():
+            final.append(items(row))
+
+        if page > total_pages + 1:
+            response = jsonify(status=422, response={
+                               'val': 'try lower page number'})
+            return '', '', response
+
+        elif page == total_pages + 1:
+            res_list = jsonify(status=200, response=[
+                               e.serialize() for e in final[total_pages * self.per_page:]], page=page,
+                               per_page=remain, total_results=total)
+            return "search.html", final[total_pages * self.per_page:], res_list
+
         res_list = []
         res_list = jsonify(status=200, response=[
                            e.serialize() for e in final[start:end]], page=page,
@@ -131,10 +168,7 @@ app_obj = Flask_app()
 
 @app.route('/')
 def home():
-    response = jsonify(status=422, response={
-                               'available api': ['/movie/${id}', '/show/${id}', '/search?query={some title}', 
-                               '/search?query={some title}&page={some page}']})
-    return response
+    return render_template('home.html')
 
 
 @app.route('/movie/<id>')
@@ -152,14 +186,18 @@ def show_id(id):
 @app.route('/search')
 def search_title():
     title = request.args['query']
-    if len(request.args) > 1:
+    if len(request.args) == 2:
         page = request.args['page']
         search_page, content, response = app_obj.search_title(title, int(page))
-        return response
     else:
         search_page, content, response = app_obj.no_page_search_title(title)
+
+    if len(request.args) == 3:
+        type = request.args['type']
+        page = request.args['page']
+        search_page, content, response = app_obj.search_title_type(
+            title, int(page), type)
     return response
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 33507))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
