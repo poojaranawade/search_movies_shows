1891d7506fa26495efb47e8fa362e39ff8fdff73
Pooja Ranawade <poojaranawade@gmail.com>
2018-10-30 11:35:31 -0700

    main

--- main.py
+++ main.py
@@ -159,4 +159,4 @@ def search_title():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True,port=port)
+    app.run(debug=True,port=port, host='0.0.0.0')
